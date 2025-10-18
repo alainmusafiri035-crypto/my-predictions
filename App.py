3dc50213107737dae6ba1136d7870f4e36183b55
alainmusafiri035-crypto <alainmusafiri035@gmail.com>
2025-10-18 13:01:16 +0200

    Add files via upload

---
+++ App.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pickle
+import numpy as np
+with open('musa.pkl','rb') as fille:
+    model=pickle.load(fille)
+
+st.title("taxi trip pricing prediction dashboard")
+st.write("Enter The following input to get prediction")
+duration=st.number_input("time to destination")
+kilometers=st.number_input("trip kilometers")
+passengers=st.number_input("number of passengers")
+
+if st.button("Submit"):
+    input_data=np.array([[duration,kilometers,passengers]])
+    result=model.predict(input_data)
+    #st.success("Predicted taxi trip pricing= ", result)
+    st.success(f"Predicted taxi trip pricing: {result[0]:,.2f}$")    
